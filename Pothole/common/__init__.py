24b266d8147d046051707be76efb14c719b80d89
320-05\수빈36 <ksb2116@gmail.com>
2026-03-12 17:52:41 +0900

    Cleanup: main branch 초기화

--- Pothole/common/__init__.py
+++
@@ -1,5 +0,0 @@
-from .session import Session
-from .ViewTracker import ViewTracker
-from .google_sheets import GoogleSheetClient
-
-__all__ = ['Session', 'ViewTracker', 'GoogleSheetClient']
--- Pothole/domain/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Member import Member
-from .Score import Score
-from .Board import Board
-from .Typing import Typing
-from .Post import Post
-
-__all__ = ['Member', 'Score', 'Board', 'Typing', 'Post']
