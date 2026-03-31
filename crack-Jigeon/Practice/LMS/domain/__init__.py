5294fc66f4c3d33f0b968950e8c1de855b72cb3f
320-14\dlwlrjs <wlrjs1300@gmail.com>
2026-03-31 12:38:09 +0900

    관리자 대시보드 수정

--- crack-Jigeon/Practice/LMS/domain/__init__.py
+++
@@ -1,8 +0,0 @@
-from .Member import Member
-from .Score import Score
-from .Board import Board
-from .UserInterestRegion import UserInterestRegion
-from .FeedTarget import FeedTarget
-from .Incident import Incident
-
-__all__ = ["Member","Score","Board","UserInterestRegion","FeedTarget","Incident"]
